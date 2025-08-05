73924983bf39bf02fcc6251b2978dffc67cde687
Julien JAMET <julienjamet1992@protonmail.com>
2025-08-05 17:39:20 +0200

    update pictures

--- src/Data.ts
+++ src/Data.ts
@@ -3,14 +3,14 @@ import { IPicture } from "./Interfaces"
 export const PictureList: IPicture[] = [
     {
         id: 1,
-        url: "https://cyber-breeze.com/wp-content/uploads/2016/11/001-12-giant-tortoise-255-years-3c2d72aa8a65d09409df959eb223af4b.jpg"
+        url: "./assets/turtle.webp"
     },
     {
         id: 2,
-        url: "https://cdn.shopify.com/s/files/1/0265/7330/7978/files/lion_1_97c111f9-135a-4f65-a717-e7c3d0b82c8c.png?v=1588177383"
+        url: "./assets/lion.webp"
     },
     {
         id: 3,
-        url: "https://img.over-blog-kiwi.com/1/23/67/19/20170708/ob_a8b133_e52-1699.jpg"
+        url: "./assets/puffin.jpg"
     }
 ]
